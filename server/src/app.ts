ac733e99d2a3cd03d07d354d90c81cdd6bcf07ec
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-07-18 14:51:40 +0530

    database setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,12 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 8000;
-
 app.get("/", (req, res) => {
   res.send("server is live!");
 });
 
-app.listen(PORT, () => {
-  console.log("Server is running on 8000");
-});
+export default app;
