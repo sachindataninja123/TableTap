a63aea8aea3d96cc5ba60eec1d906625d1e15e9c
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-07-22 22:58:28 +0530

    admin routes added

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,6 +1,21 @@
-import express from "express"
+import express from "express";
+import { adminOnly, isAuth } from "../middlewares/isAuth.middleware.js";
+import {
+  deleteUser,
+  getAllUsers,
+  getPlatformStats,
+  getUserById,
+  promoteToAdmin,
+  toggleBanUser,
+} from "../controllers/admin.controller.js";
 
-const adminRouter = express.Router()
+const adminRouter = express.Router();
 
+adminRouter.get("/users", isAuth, adminOnly, getAllUsers);
+adminRouter.get("/users/:id", isAuth, adminOnly, getUserById);
+adminRouter.delete("/users/:id", isAuth, adminOnly, deleteUser);
+adminRouter.patch("/users/:id/ban", isAuth, adminOnly, toggleBanUser);
+adminRouter.patch("/users/:id/promote", isAuth, adminOnly, promoteToAdmin);
+adminRouter.get("/stats", isAuth, adminOnly, getPlatformStats);
 
-export default adminRouter
+export default adminRouter;
