bc405ccf371d6eb30029f625b5724fdf4cffbec9
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-07-25 13:20:36 +0530

    axios setup

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1",
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "https://tabletap-backend-s04v.onrender.com/api/v1",
   withCredentials: true,
 });
 
-
-export default axiosInstance
+export default axiosInstance;
