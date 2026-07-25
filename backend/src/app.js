1fe3262c2174b1fb6ba04f13c4f81ab49b0ffaad
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-07-25 13:32:41 +0530

    cors setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://tabletap-1.onrender.com/"],
+    origin: "https://tabletap-1.onrender.com",
     credentials: true,
   }),
 );
